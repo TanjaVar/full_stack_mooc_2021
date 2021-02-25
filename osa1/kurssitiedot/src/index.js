e0f2540e42c72ca439bb2103898b4397238b3e19
Tanja <varvio.tanja@gmail.com>
2021-02-25 12:28:49 +0200

    refractoring

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,11 +1,11 @@
 /* Refaktoroi sovelluksen koodi siten, että se 
 koostuu kolmesta uudesta komponentista: Header,
- Content ja Total. Kaikki data pidetään edelleen
-  komponentissa App, joka välittää tarpeelliset 
-  tiedot kullekin komponentille props:ien avulla.
-   Header huolehtii kurssin nimen renderöimisestä,
-    Content osista ja niiden tehtävämääristä ja Total 
-    tehtävien yhteismäärästä.*/
+Content ja Total. Kaikki data pidetään edelleen
+komponentissa App, joka välittää tarpeelliset 
+tiedot kullekin komponentille props:ien avulla.
+Header huolehtii kurssin nimen renderöimisestä,
+Content osista ja niiden tehtävämääristä ja Total 
+tehtävien yhteismäärästä.*/
 
 import React from 'react';
 import ReactDOM from 'react-dom';
