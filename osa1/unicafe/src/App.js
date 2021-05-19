ff4cd4e5e3b46dd427aa21daea474b6e86108e65
Tanja Varvio <varvio.tanja@gmail.com>
2021-05-19 15:09:10 +0300

    exercise unicafe: working buttons

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,40 +1,50 @@
 import React, {useState} from 'react'
 
 const Button = (props) => {
-    <button onClick={props.handleClick}>props.name</button>
+  return (
+      <>
+        <button onClick={props.handleClick}>{props.text}</button>
+      </>
+    )
 }
+
+const CalculateAverage = (props) => {
+  const average = (props.good + props.bad)/props.pressed
+  return (
+    <div>
+      Average {average}
+    </div>
+  )
+}
+
+const CalculatePositive = (props) => {
+  const positive = (props.good/props.pressed)*100
+  return (
+    <div>
+      Positive {positive} %
+    </div>
+  )
+}
+
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const pressed = 0
-  const positivePressed = 0
-
-
-  
-  const calcAverage = () => {
-    return (
-      (good+bad)/pressed
-    )
-  } 
-  
-  const calcPositive = () => {
-    
-  }
+  const [pressed, setPressed] = useState(0)
 
   return (
     <div>
-        <h1>give feedback</h1>
-        <Button handleClick={ () => return ( setGood(good+1); pressed++; positivePressed++)} text='good'/>
-        <Button handleClick={ () => return (setNeutral(neutral+1); pressed++;)}text='neutral'/>
-        <Button handleClick={ () => return (setBad(bad+1); pressed++;)} text='bad'/>
+        <h1>Give us feedback</h1>
+        <Button handleClick={ () => { setGood(good+1); setPressed(pressed+1);}} text='good'/>
+        <Button handleClick={ () => { setNeutral(neutral+1); setPressed(pressed+1);}} text='neutral'/>
+        <Button handleClick={ () => { setBad(bad+1); setPressed(pressed+1);}}text='bad'/>
         <h1>statistics</h1>
-        good {good}
-        neutral {neutral}
-        bad {bad}
-        average {calcAverage}
-        positive {calcPositive}
+        good {good} <br />
+        neutral {neutral} <br />
+        bad {bad} <br />
+        <CalculateAverage pressed={pressed} good={good} bad={bad} neutral={neutral}/>
+        <CalculatePositive pressed={pressed} good={good} />
     </div>
   )
 }
