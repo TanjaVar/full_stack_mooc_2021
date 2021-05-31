dff658afdb4e685fe513895115e65747889a50a0
Tanja Varvio <tattitv@gmail.com>
2021-05-31 18:12:18 +0300

    part1 folder: tracking, App.js and index.js updated

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,39 +1,45 @@
 import React, {useState} from 'react'
 
 const Button = (props) => {
-    <button onClick={props.handleClick}>props.name</button>
-}
+  return (
+    <>
+      <button onClick={props.handleClick}>{props.text}</button>
+    </>
+)}
 
+// buttons: good = +1, neutral = 0, bad = -1
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const pressed = 0
-  const positivePressed = 0
+  const allClicks = 0
 
-
+  //calculates average mumber from given feedback
   const calcAverage = () => {
     return (
-      (good+bad)/pressed
+      (good+bad+neutral)/ allClicks
     )
   } 
   
+  //claculates % of positive feedback from overall feedbacks
   const calcPositive = () => {
-    
+    return (
+      (good / allClicks) * 100
+    )
   }
 
   return (
     <div>
-        <h1>give feedback</h1>
-        <Button handleClick={ () => return ( setGood(good+1); pressed++; positivePressed++)} text='good'/>
-        <Button handleClick={ () => return (setNeutral(neutral+1); pressed++;)}text='neutral'/>
-        <Button handleClick={ () => return (setBad(bad+1); pressed++;)} text='bad'/>
+        <h1>give us feedback</h1>
+        <Button handleClick={ () => { setGood(good+1)}} text='good'/>
+        <Button handleClick={ () => { setNeutral(neutral+1)}} text='neutral'/>
+        <Button handleClick={ () => { setBad(bad+1)}} text='bad'/>
         <h1>statistics</h1>
-        good {good}
-        neutral {neutral}
-        bad {bad}
-        average {calcAverage}
-        positive {calcPositive}
+        good {good} <br/>
+        neutral {neutral} <br/>
+        bad {bad} <br />
+        average {calcAverage} <br/>
+        positive {calcPositive} <br/>
     </div>
   )
 }
