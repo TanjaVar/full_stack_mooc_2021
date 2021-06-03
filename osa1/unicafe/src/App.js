0c97aefbf796f37d36a03a6d36642b45025c812c
Tanja Varvio <tattitv@gmail.com>
2021-06-03 15:57:28 +0300

    unicafe exercise

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -29,17 +29,23 @@ const CalculatePositive = (props) => {
 // 1.8 unicafe step 3
 // includes all functions which return statistics of the feedback application
 const Statistics = (props) => {
-  return (
-    <div>
-        <h1>statistics</h1>
+  if ({props.pressed} > 0) {
+    return (
+      <div>
         good {props.good} <br />
         neutral {props.neutral} <br />
         bad {props.bad} <br />
         <CalculateAverage pressed={props.pressed} good={props.good} bad={props.bad} neutral={props.neutral}/>
         <CalculatePositive pressed={props.pressed} good={props.good} /> 
-    </div>
-  )
-
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
 }
 
 const App = () => {
@@ -54,6 +60,7 @@ const App = () => {
         <Button handleClick={ () => { setGood(good+1); setPressed(pressed+1);}} text='good'/>
         <Button handleClick={ () => { setNeutral(neutral+1); setPressed(pressed+1);}} text='neutral'/>
         <Button handleClick={ () => { setBad(bad+1); setPressed(pressed+1);}}text='bad'/>
+        <h1>statistics</h1>
         <Statistics pressed={pressed} good={good} bad={bad} neutral={neutral}/>
     </div>
   )
