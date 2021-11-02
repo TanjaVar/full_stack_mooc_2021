81aa4e29153dff9113a9aa862b4d8f9476ab291d
Tanja Varvio <varvio.tanja@gmail.com>
2021-11-02 14:03:24 +0200

    Kokoelmien renderointi ja moduulit exercise 2.1

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import Header from './Header.js';
-import Content from './Header.js';
 
- 
-//komponentti Course sisältää edellisessä osassa määritellyt komponentit, joiden vastuulle tulee kurssin nimen ja osien renderöinti.
-const Course = (props) => {
-
+const Header = (props) => {
     return (
         <>
-
+            <h1>{props.courseHeader}</h1>
         </>
     )
 };
+
+
+const Content = (props) => {
+    console.log("Content props:",props)
+    return (
+        <>  
+            {props.courseContent.map(part => 
+                <Part name={part.name} exercises={part.exercises} key={part.id}/>
+            )}
+        </>
+    )
+};
+
+const Part = ({name, exercises }) => {
+    console.log("PART:",  name)
+    console.log("EERCISES:", exercises)
+    return (
+        <>
+            <p> {name} {exercises}</p>
+        </>
+    )
+};
+
+
+//komponentti Course sisältää edellisessä osassa määritellyt komponentit, joiden vastuulle tulee kurssin nimen ja osien renderöinti.
+const Course = ({ course }) => {
+    console.log("WOLOLO", course) // undefined
+
+    return (
+        <>
+            <Header courseHeader={course.name}/>
+            <Content courseContent={course.parts}/>
+        </>
+    )
+}
 
 export default Course;
