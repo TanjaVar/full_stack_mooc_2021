4b1c85070deb0b909b79c4cdbdf068779c5c5d8f
Tanja Varvio <varvio.tanja@gmail.com>
2021-11-08 16:33:39 +0200

    Lomakkeiden kasittely: 2.7 puhelinluettelo step2

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,42 +2,58 @@ import React, {useState} from 'react';
 
 //Tila newName on tarkoitettu lomakkeen kentän kontrollointiin.
 //Toteutetaan tässä tehtävässä henkilön lisäys puhelinluetteloon.
+const ListGenerator = (props) => {
+  //console.log(props, 'listgenerator')
+  return (
+    <li>{props.person.name}</li>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Tanja Varvio'}
+    {name: 'Tanja Varvio'},
+    {name: 'tatti'}
   ]);
   const [newName, setName] = useState('type name here');
 
   //kutsutaan kun buttonia painetaan
   const addName = (event) => {
     event.preventDefault(); // estää oletustoiminnan onClick event
-
     const personObject = {
       name: newName
     };
 
-    //asettaa uuden listan jossa päiviretty uusi alkio mukana
-    setPersons(persons.concat(personObject));
+    //looppaa arrayn läpi ja jos löytää nimen sieltä niin sitten alert
+    // muuttaa 
+    let nameFound = false;
+    for (let i = 0; i < persons.length; i++) {
+      //console.log(newName)
+      //console.log(persons[i].name)
+      //console.log(persons.length)
+      if (newName === persons[i].name) {
+        window.alert(`${persons[i].name} is already in PHONEBOOK`)
+        nameFound = true
+      } 
+    }
+
+    if(!nameFound) {
+      setPersons(persons.concat(personObject))
+    }
+
     // nollaa newName State
-    setName(' ')
+    setName('')
   }
 
   //kutsutaan kun teksti name kentäsä vaihtuu
   const handleTextChange = (event) => {
+    console.log(event.target.value)
     setName(event.target.value)
-  }
-
-  const ListGenerator = (props) => {
-    //console.log(props, 'listgenerator')
-    return (
-      <li>{props.person.name}</li>
-    )
   }
 
 
   return (
     <div>
+      <h1>PHONEBOOK</h1>
       <form onSubmit={addName}>
         <div>
           name: 
