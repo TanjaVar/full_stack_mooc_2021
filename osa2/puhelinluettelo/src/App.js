26a40dd43c751e145f2963a4c06a3104173b9015
Tanja Varvio <varvio.tanja@gmail.com>
2022-02-04 11:57:29 +0200

    add filter functionality

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,43 +1,78 @@
 import React, {useState} from 'react';
 
-//Tila newName on tarkoitettu lomakkeen kentän kontrollointiin.
-//Toteutetaan tässä tehtävässä henkilön lisäys puhelinluetteloon.
+// checks if persons array has same name as the user typed to the filter input
 const ListGenerator = (props) => {
-  //console.log(props, 'listgenerator')
+  //console.log('ListGenerator: tuleeko filtteri sana tänne nyt? true!', props.filterWith);
+  //console.log('ListGenerator: tuleeko false',props.loytyy)
   return (
-    <li>{props.person.name} {props.person.phonenum}</li>
+    <li>{props.person.name} {props.person.phonenum} </li>
   )
+
+  // //if true -> returns name and phonenum, if false => returns null
+  // if (props.person.name.toLowerCase() === props.filterWith.toLowerCase()) {
+  //   return (
+  //     <RowGenerator name={props.person.name} phonenum={props.person.phonenum}/>
+  //   )
+  //   }else {
+  //     return (
+  //       null
+  //     )
+  //   }
 }
 
+// // returns name and phonenum in <li> element
+// const RowGenerator = (props) => {
+//   return (
+//     <li>{props.name} {props.phonenum}</li>
+//   )
+// }
+
 const App = () => {
+
+  //states
+  const [filterWith, setFilterWith] = useState('')
   const [persons, setPersons] = useState([
     {
-      name: 'Tanja Varvio',
+      name: 'Tanja',
       phonenum: '056 943 94343'
     },
     {
       name: 'tatti',
       phonenum: '456 0323 9323'
+    },
+    {
+      name: 'Make',
+      phonenum: '04363465346346'
+    },
+    {
+      name: 'Taateli',
+      phonenum: '34653 363643'
+    },
+    {
+      name: 'Puuteli',
+      phonenum: '324 4235 52352'
     }
   ]);
-  const [newName, setName] = useState('type name here');
-  const [newPhonenumber, setNewPhonenumber] = useState('type number here');
+  const [newName, setNewName] = useState('');
+  const [newPhonenumber, setNewPhonenumber] = useState('');
+  //const [loytyy, setLoytyy] = useState(false);
+  //const [filtteroity, setFiltteroity] = useState(persons) //by default has persons array
 
-  //kutsutaan kun buttonia painetaan
+
+
+  // function is called when 'submit' button is pressed on UI
   const addName = (event) => {
-    event.preventDefault(); // estää oletustoiminnan onClick event
+    event.preventDefault(); // prevent default onClick event
+
+    //sets input values to object
     const personObject = {
       name: newName,
       phonenum: newPhonenumber
     };
-    //looppaa arrayn läpi ja jos löytää nimen sieltä niin sitten alert
-    // muuttaa 
+
     let nameFound = false;
     for (let i = 0; i < persons.length; i++) {
-      //console.log(newName)
-      //console.log(persons[i].name)
-      //console.log(persons.length)
-      if (newName === persons[i].name) {
+      if (newName.toLowerCase() === persons[i].name.toLowerCase()) {
         window.alert(`${persons[i].name} is already in PHONEBOOK`)
         nameFound = true
       } 
@@ -45,25 +80,88 @@ const App = () => {
     if(!nameFound) {
       setPersons(persons.concat(personObject))
     }
-    // nollaa newName State
-    setName('')
+    
+
+    // sets statevalues empty after new object has been added
+    setNewName('')
+    setNewPhonenumber('')
   }
+
 
   //kutsutaan kun teksti name kentäsä vaihtuu
   const handleTextChange = (event) => {
-    console.log(event.target.value)
-    setName(event.target.value)
+    console.log('handleTextChange: ', event.target.value)
+    setNewName(event.target.value)
   }
+
+
 
   // updating setPhonenumber every time new char is typed to the input
   const handlePhonenumberChange = (event) => {
-    console.log(event.target.value)
+    console.log('handlePhonenumberChange: ', event.target.value)
     setNewPhonenumber(event.target.value)
   }
 
 
+ 
+  // // updating filterWith every time new char is typed to the filter input
+  // const handleFilterWith = (event) => {
+  //   console.log('handleFilterwith: ', event.target.value);
+  //   //jos phonebookista löytyy annettu nimi, niin loytyy state muuttuu true si, muutoin false
+  //   //joka kerta jokaisen kirjoitetun hakusanan jälkeen appi katsoo onko phonebookissa jo sama
+  //   // for (let i = 0; i < persons.length; i++){
+  //   //   if (persons[i].name.toLowerCase()===event.target.value.toLowerCase()) {
+  //   //     setLoytyy(true)
+  //   //   }
+  //   // }
+
+  //   // //toimii kuten pitää
+  //   // console.log('handleFilterWith: jos kirjoitat tatti niin tuleeko true', loytyy, event.target.value.toLowerCase())
+
+  //   // //if name is in array then set the name in filtteroity
+  //   // if (setLoytyy===true) {
+  //   //   setFiltteroity(persons.filter(person => person.name.toLowerCase()===event.target.value.toLowerCase()))
+  //   // }
+
+  //   // //if name isn't in array then set persons array content to filtteroity state
+  //   // if (setLoytyy===false) {
+  //   //   setFiltteroity(persons)
+  //   // }
+
+  //   // console.log('handlefilterWith: tuleeko paivitetty lista yhdestä henkilöstä joka kirjoitettiin filtteriin? ', filtteroity)
+
+  //   // // looppaa persons listan läpi ja jos listasta löytyy sama nimi minkä käytätjä on antanut niin filtteröi kyseisen objektin setFiltteröity taulukkoon.
+  //   // persons.map(person => {
+  //   //   if(person.name === event.target.value) {
+  //   //     return (
+  //   //       setFiltteroity(persons.filter(person => person.name===event.target.value))
+  //   //     )
+  //   //   }else {
+  //   //     return (
+  //   //     setFiltteroity(persons)
+  //   //     )
+  //   // }})
+
+  //   //sets state
+  //   setFilterWith(event.target.value);
+  //   console.log('handlFilterWith ', filterWith)
+  // }
+
+
+
+
   return (
     <div>
+      filter shown with:
+      <input 
+        type="text" 
+        placeholder="search..." 
+        onChange={(event) => { 
+          console.log('tuleeko tahan annettu filtterisana? tuleeko string? Funktio sanoo:', event.target.value,  typeof event.target.value)
+          return (
+            setFilterWith(event.target.value)
+          )}}
+      />
       <h1>PHONEBOOK</h1>
       <form onSubmit={addName}>
         <div>
@@ -84,16 +182,23 @@ const App = () => {
           <button type="submit">Add contact</button>
         </div>
       </form>
-      <h2>Numbers/Names</h2>
-      <ul>
-          {persons.map( person => 
+      <h2>NAMES - NUMBERS</h2>
+      <ul> 
+        {persons.filter((person) => {
+          console.log('persons filtering: ', person, person.name, filterWith)
+          if (filterWith==="") {
+            return person
+          } else if (person.name.toLowerCase()===filterWith.toLowerCase()) {
+            return person
+          }
+        }).map(person => {
+          return (
             <ListGenerator person={person} key={person.name} />
-          )}
+          )
+        })}
       </ul>
     </div>
   )
 };
 
 export default App;
-
-// puh nro ei päivity newPhonenum muuttujaan oncahnge metodilla inputissa, miksei?
