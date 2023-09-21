5eb46c5472306d1366533ef5d22cfcf55c200e17
Tanja Varvio <tanja.varvio@koud.fi>
2023-09-21 12:32:51 +0300

    Add json data

--- osa3/backend-exercise/index.js
+++ osa3/backend-exercise/index.js
@@ -1,5 +1,22 @@
 const http = require("http")
 
+let notes = [
+	{
+		id: 1,
+		content: "HTML is easy",
+		important: true
+	},
+	{
+		id: 2,
+		content: "Browser can execute only JavaScript",
+		important: true
+	},
+	{
+		id: 3,
+		content: "GET and POST are the most important methids id HTTP protocol",
+		important: true
+	}
+]
 const app = http.createServer((request, response) => {
 	response.writeHead(200, { 'Content-Type': 'text/plain'})
 	response.end('Hello World')
