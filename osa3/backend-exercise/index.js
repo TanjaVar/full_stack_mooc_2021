ea463c313f3da83ed5d96d45870d9ec78bdb0a5b
Tanja Varvio <tanja.varvio@koud.fi>
2023-09-25 13:17:25 +0300

    Change prop names

--- osa3/backend-exercise/index.js
+++ osa3/backend-exercise/index.js
@@ -20,8 +20,8 @@ let notes = [
 	}
 ]
 
-app.get('/', (req, res) => {
-	res.send('<h1>Hello World!</h1>')
+app.get('/', (request, response) => {
+	response.send('<h1>Hello World!</h1>')
 })
 
 app.get('api/notes', (req, res) => {
