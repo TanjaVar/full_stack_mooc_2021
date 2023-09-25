c9ba015eb568c7b61958301015d3e22f189fd511
Tanja Varvio <tanja.varvio@koud.fi>
2023-09-25 13:24:02 +0300

    Add console log when listening port

--- osa3/backend-exercise/index.js
+++ osa3/backend-exercise/index.js
@@ -30,4 +30,6 @@ app.get('/api/notes', (req, res) => {
 })
 
 const PORT = 3001
-app.listen(PORT, () => {	console.log(`Server running in port ${PORT}`)})
+app.listen(PORT, () => {
+	console.log(`Server running in port ${PORT}`)
+})
