0c2b363785d17cf292773ee2a07511d6e5de5037
Tanja Varvio <tanja.varvio@koud.fi>
2023-09-26 15:30:09 +0300

    Change HTTP GET request handlers path

--- osa3/backend-exercise/index.js
+++ osa3/backend-exercise/index.js
@@ -29,7 +29,7 @@ app.get('/', (request, response) => {
 })
 
 //handles HTTP GET requests
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes', (req, res) => {
 	const id = Number(req.params.id)
 	// console.log(id)
 	const note = notes.find(note => note.id === id)
