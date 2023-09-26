519b24ba209a4745068c9ad52bdb3372e0429bef
Tanja Varvio <tanja.varvio@koud.fi>
2023-09-26 10:02:53 +0300

    Add if else statement for get response

--- osa3/backend-exercise/index.js
+++ osa3/backend-exercise/index.js
@@ -27,6 +27,17 @@ app.get('/', (request, response) => {
 //handles HTTP GET requests
 app.get('/api/notes', (req, res) => {
 	res.json(notes)
+app.get('/api/notes/:id', (req, res) => {
+	const id = Number(req.params.id)
+	// console.log(id)
+	const note = notes.find(note => note.id === id)
+	// console.log(note)
+	if (note) {
+		response.json(note)
+	} else {
+		response.status(404).end()
+	}
+})
 })
 
 const PORT = 3001
