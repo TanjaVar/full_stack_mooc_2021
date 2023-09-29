7a65ad5179303d6787320f9807234ee4001a5b4c
Tanja Varvio <tanja.varvio@koud.fi>
2023-09-29 10:53:55 +0300

    Fix HTTP GET route path and prop usage

--- osa3/backend-exercise/index.js
+++ osa3/backend-exercise/index.js
@@ -45,8 +45,8 @@ app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>')
 })
 
-app.get('api/notes', (request, response) => {
-	res.json(notes)
+app.get('/api/notes', (request, response) => {
+	response.json(notes)
 })
 
 //handles HTTP GET requests
