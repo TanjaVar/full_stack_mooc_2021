ba5480db3568ec01bda8e39a8bdf96c6f25ac18c
Tanja Varvio <tanja.varvio@koud.fi>
2023-12-11 08:58:09 +0200

    Add Material UI components and cleanup

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,8 +4,8 @@ import Filter from './components/Filter';
 import Form from './components/Form';
 import phonebookService from './services/phonebookPersons'
 import Notification from './components/Notification'
+import Card from '@mui/material/Card';
 
-//Exercise 2.11 working properly
 const App = () => {
   //states
   const [filterWith, setFilterWith] = useState('')
@@ -14,141 +14,129 @@ const App = () => {
   const [persons, setPersons] = useState([])
   const [notificationMessage, setNotificationMessage] = useState('')
 
-  //use effect function which gets data from database and loads axios method only once when component loads
+  // use effect function which gets data from database and loads axios method only once when component loads
   useEffect(() => {
-    console.log('effect')
     phonebookService
     .getAll()
     .then(response => {
-      console.log('promise fulfilled! data fetched from database')
       setPersons(response.data)
     })
   }, [])
-  console.log('render', persons.length, 'persons')
 
-  // function is called when 'submit' button is pressed on UI
-  const addName = (event) => {
+  const addName = ( event ) => {
     event.preventDefault(); // prevent default onClick event
-    console.log("Event: ", event.target) // DEBUG
 
-    //sets input values to object
-    const personObject = {
-      name: event.target.newName,
-      phonenum: event.target.newPhonenumber
-    };
-    console.log("newPhonenumber: ", personObject.newPhonenumber) // DEBUG
+      //sets form input values to new object
+      const personObject = {
+        name: newName,
+        phonenum: newPhonenumber
+      };
 
     let nameFound = false;
     for (let i = 0; i < persons.length; i++) {
-      //console.log(newName) // DEBUG 
-      if (newName.toLowerCase() === persons[i].name.toLowerCase()) {
+      if (personObject.name.toLowerCase() === persons[i].name.toLowerCase()) {
         if (window.confirm(
           `${personObject.name} 
           and his/her phonenumber is already in phonebook. Do you wish to overwrite old number with new one?`
           )) {
-          // find person who's phonenumber is going to be overwrited
-          const person = persons.find(person => person.name === personObject.name)
-          console.log("person found: ", person) // DEBUG
-          // copy const person,but change it with new phonenumber
-          const changedPerson = {...person, phonenum: personObject.newPhonenumber}
-          console.log("changedPerson: ", changedPerson) // DEBUG
 
-          // call phonebookservices updatePhoneNum function and update database
           phonebookService
-          .updatePhoneNum(personObject.id, personObject)
+          .updatePhoneNum(persons[i].id, personObject)
           .then( response => {
-            console.log(`AddName: ${personObject.name} new phonenum was successfully added to phonebook`)
-            setPersons(persons.map(person => person.name.toLowerCase() !== personObject.name.toLowerCase() ? person : response.data))
-/*             setNotificationMessage(`${personObject.name} new phonenum was successfully added to phonebook`)
+            setPersons(persons.map(p => p.name.toLowerCase() !== personObject.name.toLowerCase() ? p : response.data))
+            setNotificationMessage(`${personObject.name} new phonenum was successfully added to phonebook`)
             setTimeout( () => {
               setNotificationMessage(null)
-            }, 4000)  */
+            }, 4000)
           })
           .catch(error => {
             window.alert(`something went wrong while trying to update phonenumber`)
           })
         }
-        //window.alert(`${persons[i].name} is already in PHONEBOOK`)
         nameFound = true
-      } 
+      }
     }
+
     if(!nameFound) {
       phonebookService
         .create(personObject)
         .then(response => {
           setPersons(persons.concat(response.data))
-          console.log(`addName function: new contact sended successfully`) //check from conosle that sent information is correct
         })
     }
-    // sets statevalues empty after new object has been added
     setNewName('')
     setNewPhonenumber('')
   }
 
-  //eventHandler: called when text in "name" field is changed
+  // Handles newName state
   const handleTextChange = (event) => {
-    console.log('handleTextChange: ', event.target.value)
-    setNewName(event.target.newName)
+    setNewName(event.target.value)
   }
 
-  // eventhandler: updating setPhonenumber every time new char is typed to the input
+  // Handles phonenumber state
   const handlePhonenumberChange = (event) => {
-    console.log('handlePhonenumberChange: ', event.target.value)
-    setNewPhonenumber(event.target.newPhonenumber)
+    setNewPhonenumber(event.target.value)
   }
 
-  // eventHandler: updating filterWith state
+  // Handles filterWith state
   const handleFilterChange = (newValue) => {
     setFilterWith(newValue)
   }
 
-  //deletes person from Phonebook. gets unique id and person name as parameters
-  const deletePerson = ( id, name ) => {
-    console.log('deletePerson function: Im working!')
-    console.log(`deletePerson: ${id} ${name}`)
-
+  const deletePerson = (id, name) => {
     if (window.confirm(`Are you sure you want to delete person ${name} from phonebook?`)) {
       phonebookService
       .deleteFromPhonebook(id)
       .then(promise => {
-        console.log('deletePerson function: promise fulfilled yay')
-        setPersons(persons.map(person => person.id !== id ? person : promise.data))
-/*         setNotificationMessage("Person deleted!")
+        setPersons(persons.map(p => p.id !== id ? p : promise.data))
+        setNotificationMessage("Person deleted!")
         setTimeout( () => {
           setNotificationMessage(null)
-        }, 4000) */
+        }, 4000)
       })
       .catch(error => {
-/*         setNotificationMessage("something went wrong")
+         setNotificationMessage("something went wrong")
           setTimeout( () => {
             setNotificationMessage(null)
-          }, 4000) */
+          }, 4000)
       })
     }
   }
 
   return (
     <div>
-      <Notification message={notificationMessage}/>
-      <Filter filterWith={filterWith} onChange={handleFilterChange}/>
-      <h1>PHONEBOOK</h1>
-      <h3>Add a name</h3>
-      <Form addName={addName} newName={newName} handleTextChange={handleTextChange} newPhonenumber={newPhonenumber} handlePhonenumberChange={handlePhonenumberChange}/>
-      <h2>NAME - NUMBER</h2>
-      <ul>
-        {persons.filter((person) => {
-          // console.log('persons filtering: ', person, person.name, filterWith)
-          if (filterWith==="") {
-            return person
-          } else if (person.name.toLowerCase()===filterWith.toLowerCase()) {
-            return person
-          }
-        }).map(person => {
-          return (
-            <ListGenerator person={person} key={person.id} deletePersonFromPhonebook={() => deletePerson(person.id, person.name)}/>
-          )
-        })}
-      </ul>
+      <Card variant="outlined" sx={{borderRadius: '5%', margin: '10px', padding: '100px', backgroundColor: 'lightgray'}}>
+        <Notification message={notificationMessage}/>
+        <h1>PHONEBOOK</h1>
+        <h3>Add name</h3>
+        <Card sx={{borderRadius: '5%', margin: '10px', padding: '20px'}}>
+          <Form 
+            addName={addName} 
+            newName={newName} 
+            handleTextChange={handleTextChange} 
+            newPhonenumber={newPhonenumber} 
+            handlePhonenumberChange={handlePhonenumberChange}
+          />
+        </Card>
+        <Filter filterWith={filterWith} onChange={handleFilterChange}/>
+        <h2>NAME - NUMBER</h2>
+
+          {persons.filter((person) => {
+            if (filterWith === "") {
+              return person
+            } else if (person.name.toLowerCase() === filterWith.toLowerCase()) {
+              return person
+            }
+          }).map(person => {
+            return (
+              <ListGenerator 
+                person={person} 
+                key={person.id} 
+                deletePersonFromPhonebook={() => deletePerson(person.id, person.name)}
+              />
+            )
+          })}
+      </Card>
     </div>
   )
 };
