6946aba2b37bf23c8685f4efc69559c4c0d42d65
Tanja Varvio <tanja.varvio@koud.fi>
2023-12-11 08:57:26 +0200

    Add Material ui components

--- osa2/puhelinluettelo/src/components/ListGenerator.js
+++ osa2/puhelinluettelo/src/components/ListGenerator.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Card from '@mui/material/Card'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
 const ListGenerator = ({ person, deletePersonFromPhonebook }) => {
     return (
-        <li>
-            {person.name} {person.phonenum} <button onClick={deletePersonFromPhonebook}>delete</button>
-        </li>
+        <>
+            <Grid container spacing={2} variant="outlined">
+                <Grid item xs={4}>
+                    <p class="phonebookitem">{person.name}</p>
+                </Grid>
+                <Grid item xs={4}>
+                    <p class="phonebookitem">{person.phonenum}</p>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button onClick={deletePersonFromPhonebook} variant="outlined" color="error" sx={{margin: "6px"}}>delete</Button>
+                    {/* <button class="delete" onClick={deletePersonFromPhonebook}>delete</button>*/}
+                </Grid>
+            </Grid>
+        </>
     )  
 }
 
